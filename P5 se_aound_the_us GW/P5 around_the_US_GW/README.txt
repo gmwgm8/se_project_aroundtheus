8b22d90e110ec1abcbbc53984961738a49f63285
gmwgm8 <95301069+gmwgm8@users.noreply.github.com>
2025-07-10 09:57:58 -0500

    Add files via upload
    Signed-off-by: gmwgm8 <95301069+gmwgm8@users.noreply.github.com>

---
+++ P5 se_aound_the_us GW/P5 around_the_US_GW/README.txt
@@ -0,0 +1,61 @@
+Triple Ten Project 4 Stage 2 "Around The US"
+
+Project 3 Walkthrough
+https://drive.google.com/file/d/139kcQdMhVcHj6eFEcsLL6P6sMpX-CInc/view?usp=sharing
+
+GitHub repository:
+https://github.com/gmwgm8/AroundTheUS_FINAL.git
+
+
+Rough Description:
+
+This project implemented adaptive tools to allow the website to adjust to three different screen sizes.
+The desire was to smoothly transition from Desktop, Tablet to Mobile while shrinking the browser window. 
+To accomplish this, we utilized the @mediaquery tool and specified the screen size limits for Tablet and Mobile only. 
+This is because Desktop screen size is the default. We also utilized Grid instead of Flexbox to allow the cards of the same size
+to fall into desired alignment and spacing. We used Figma for the first time in detecting the desired margins and spacing of website features within desktop
+and mobile screen sizes, tablet spaces were more or less inherited from the desktop styles along with a bit of my own determinations. 
+Using Figma I did encounter some issues with calling out too many margins and spaces, but tutors did help me to weed these out. 
+
+Now in Sprint 4 we've begun implementing Javascript using the tools "Array" and "Object" which are on small display here. 
+The start of responsive modal (pop-up form) to allow editing of the name and description of the around the world site. 
+Also, a @mediaquery to modify this popup in a mobile screen size.
+
+Javascipt was implemented to open and close the Responsive Modal used to edit the profile. In the final stage we've begun using javascript to prevent the page
+from reloading upon clicking the save button in the modal as well beginning work on creating and editing new cards for the profile from a template.
+
+
+07JUL25
+
+The updated form of the around the US site has five new functions:
+
+Create a new card with an image url & name.
+
+The ability to delete old and new cards by clicking on the trash icon.
+
+Clicking on the image on each card will give the user an enlarged picture preview with the site darkened in the background. Clicking on the X will close the preview.
+
+When clicking all of the modals (preview, edit profile, create card) they smoothly fade into and out of view. 
+
+Clicking the heart button to "Like" a particular place. 
+
+
+
+I will attach photos that highlight these functions.
+
+
+New cards:
+
+Place: Mammoth Caverns
+
+Image URL: https://images.unsplash.com/photo-1654179859807-619e4d0201f4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1hbW1vdGglMjBDYXZlcm5zfGVufDB8fDB8fHww
+
+Place: Sedona
+
+Image URL: https://images.unsplash.com/photo-1592249797396-311ed9d417f3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8U2Vkb25hfGVufDB8fDB8fHww
+
+Place: Mesa Verde
+
+Image URL: https://images.unsplash.com/photo-1581540529586-e28d24840327?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8TWVzYSUyMFZlcmRlfGVufDB8fDB8fHww
+
+
---
+++ P5 se_aound_the_us GW/P5 around_the_US_GW/scripts/index.js
@@ -0,0 +1,151 @@
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+];
+
+const cardTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".card");
+
+// Wrappers
+
+const cardsWrap = document.querySelector(".cards__list");
+const editProfileModal = document.querySelector("#profile-edit-modal");
+const addCardModal = document.querySelector("#add-card-modal");
+const previewImageModal = document.querySelector("#preview-image-modal");
+const profileFormElement = editProfileModal.querySelector(".modal__form");
+const addCardFormElement = addCardModal.querySelector(".modal__form");
+
+// Buttons and other DOM nodes
+
+const profileEditButton = document.querySelector(".profile__edit-button");
+const profileModalCloseButton = editProfileModal.querySelector(".modal__close");
+const addCardModalCloseButton = addCardModal.querySelector(".modal__close");
+const previewModalCloseButton =
+  previewImageModal.querySelector(".modal__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const addNewCardButton = document.querySelector(".profile__add-button");
+
+//preview modal
+
+const previewImage = previewImageModal.querySelector(".modal__preview-image");
+const previewCaption = previewImageModal.querySelector(
+  ".modal__preview-caption"
+);
+
+// Form data
+
+const nameInput = profileFormElement.querySelector(".modal__input_type_name");
+const jobInput = profileFormElement.querySelector(
+  ".modal__input_type_description"
+);
+
+const cardTitleInput = addCardFormElement.querySelector(
+  ".modal__input_type_title"
+);
+const cardUrlInput = addCardFormElement.querySelector(".modal__input_type_url");
+
+function closeModal(modal) {
+  modal.classList.remove("modal__is-opened");
+}
+
+function openModal(modal) {
+  modal.classList.add("modal__is-opened");
+}
+
+function renderCard(cardData, wrapper) {
+  const cardElement = getCardElement(cardData);
+  wrapper.prepend(cardElement);
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+  closeModal(editProfileModal);
+}
+
+function handleAddCardFormSubmit(evt) {
+  evt.preventDefault();
+  const name = cardTitleInput.value;
+  const link = cardUrlInput.value;
+  renderCard({ name, link }, cardsWrap);
+  closeModal(addCardModal);
+}
+function handleImageClick(data) {
+  previewImage.src = data.link;
+  previewImage.alt = data.name;
+  previewCaption.textContent = data.name;
+  openModal(previewImageModal);
+}
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const likeButton = cardElement.querySelector(".card__like-button");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  deleteButton.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
+  cardImage.addEventListener("click", () => {
+    handleImageClick(data);
+  });
+
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card__like-button_active");
+  });
+
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+  return cardElement;
+}
+// Form listeners
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+addCardFormElement.addEventListener("submit", handleAddCardFormSubmit);
+
+profileEditButton.addEventListener("click", () => {
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileDescription.textContent;
+  openModal(editProfileModal);
+});
+
+profileModalCloseButton.addEventListener("click", () =>
+  closeModal(editProfileModal)
+);
+
+// add new card
+
+addNewCardButton.addEventListener("click", () => openModal(addCardModal));
+addCardModalCloseButton.addEventListener("click", () =>
+  closeModal(addCardModal)
+);
+previewModalCloseButton.addEventListener("click", () =>
+  closeModal(previewImageModal)
+);
+initialCards.forEach((cardData) => renderCard(cardData, cardsWrap));
